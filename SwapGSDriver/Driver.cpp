cd52ca00ae171dd7d46b75f70a513e4d8aaebb2b
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2024-02-25 22:39:21 +0700

    feat: update dbg log

--- SwapGSDriver/Driver.cpp
+++ SwapGSDriver/Driver.cpp
@@ -22,9 +22,9 @@ EXTERN_C NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING Regis
 
 	UINT64 old_kernel_gs_base = 0, old_gs_base = 0, new_kernel_gs_base = 0, new_gs_base = 0;
 	my_swapgs(&old_gs_base, &old_kernel_gs_base);
-	DbgPrintEx(0, 0, "SwapGSDriver::DriverEntry-> old_gs_base: %p, old_kernel_gs_base: %p\n", old_gs_base, old_kernel_gs_base);
-
 	my_swapgs(&new_gs_base, &new_kernel_gs_base);
+
+	DbgPrintEx(0, 0, "SwapGSDriver::DriverEntry-> old_gs_base: %p, old_kernel_gs_base: %p\n", old_gs_base, old_kernel_gs_base);
 	DbgPrintEx(0, 0, "SwapGSDriver::DriverEntry-> new_gs_base: %p, new_kernel_gs_base: %p\n", new_gs_base, new_kernel_gs_base);
 
 	return STATUS_SUCCESS;
